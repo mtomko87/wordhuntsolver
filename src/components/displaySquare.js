0c2eb7a338a5ed45f9c0b25db866b65f4f9faa02
Matt <mtomko87@gmail.com>
2021-08-23 16:27:04 -0400

    disabled warning

--- src/components/displaySquare.js
+++ src/components/displaySquare.js
@@ -16,6 +16,7 @@ const DisplaySquare = props => {
         setPos();
         window.addEventListener("resize", setPos);
         return () => window.removeEventListener("resize", setPos)
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
     return (
