f9f1034bbacf0ad93cb8719c2dc80b2b2d36b32c
Matt <mtomko87@gmail.com>
2021-08-25 12:22:56 -0400

    made lines responsive

--- src/components/line.js
+++ src/components/line.js
@@ -6,18 +6,20 @@ const Line = props => {
     const line = useRef(null);
     useEffect(() => {
 
+        const offset = line.current.clientHeight / 2;
+
         const initialLength = Math.sqrt((props.end.x - props.start.x) ** 2 + (props.end.y - props.start.y) ** 2);
         const normalX = (props.end.x - props.start.x) / initialLength;
         const normalY = (props.end.y - props.start.y) / initialLength;
-        const start = {x: props.start.x - (normalX * 6), y: props.start.y - (normalY * 6)};
-        const end = {x: props.end.x + (normalX * 6), y: props.end.y + (normalY * 6)};
+        const start = {x: props.start.x - (normalX * offset), y: props.start.y - (normalY * offset)};
+        const end = {x: props.end.x + (normalX * offset), y: props.end.y + (normalY * offset)};
     
         const length = Math.sqrt((end.x - start.x) ** 2 + (end.y - start.y) ** 2);
         const angle = Math.atan2(end.y - start.y, end.x - start.x);
         const midpointX = (start.x + end.x) / 2;
         const midpointY = (start.y + end.y) / 2;
         const posX = midpointX - (length / 2);
-        const posY = midpointY - 6;
+        const posY = midpointY - offset;
 
         const element = line.current;
         element.style.width = length + "px";
